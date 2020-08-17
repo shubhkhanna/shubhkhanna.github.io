d3685d30808fea31912b9146c912c82b5d69392c
shubhkhanna <khannashubh04@gmail.com>
2020-08-18 03:04:02 +0530

    Added preloader & navbar

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// Smooth Scrolling
+$("#main-nav a").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+// Preloader
+$(window).on("load", function () {
+  if ($("#preloader").length) {
+    $("#preloader")
+      .delay(800)
+      .fadeOut("slow", function () {
+        $(this).remove();
+      });
+  }
+});
